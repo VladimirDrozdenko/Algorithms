571eb3ac4b48f7c276d8140847ff4814419ef4d2
Vladimir Drozdenko <vdrozdenko@live.com>
2015-12-28 11:34:54 -0800

    Fix algorithm and added some test code.

--- Coins1/Source.cpp
+++ Coins1/Source.cpp
@@ -1,16 +1,18 @@
 #include <algorithm>
 #include <iostream>
-#include <numeric>
+#include <iterator>
 #include <vector>
 using namespace std;
 
-int main()
+size_t coinChange(vector<size_t>& coins, size_t amount)
 {
-	size_t amount = 11;
-	vector<size_t> coins = {1, 3, 5};
+	const size_t infinity = (size_t)-1;
+	if (amount == 0)
+		return 0;
+
 	sort(coins.begin(), coins.end());
 
-	vector<size_t> min_arr(amount+1, numeric_limits<size_t>::max());
+	vector<size_t> min_arr(amount + 1, infinity);
 	min_arr[0] = 0;
 
 	for (size_t i = 1; i <= amount; ++i)
@@ -19,10 +21,31 @@ int main()
 		{
 			if (coins[c] > i)
 				break;
-			
-			min_arr[i] = min(min_arr[i], min_arr[i - coins[c]] + 1);
+
+			if (min_arr[i - coins[c]] != infinity)
+				min_arr[i] = min(min_arr[i], min_arr[i - coins[c]] + 1);
 		}
 	}
 
-	cout << min_arr[amount] << endl;
+	return (min_arr[amount] == 0 || min_arr[amount] == infinity)
+		? -1
+		: min_arr[amount];
+}
+
+int main()
+{
+	vector<size_t> coins = { 3, 5 };
+	cout << "Available coins: { ";
+	copy(coins.begin(), coins.end(), ostream_iterator<size_t>(cout, " "));
+	cout << "}\n" << endl;
+
+	for (size_t amount = 0; amount <= 20; ++amount)
+	{
+		size_t numCoins = coinChange(coins, amount);
+
+		if (numCoins == (size_t)-1)
+			cout << "  <FAILED> Amount [" << amount << "] cents cannot be constructed with given coins." << endl;
+		else
+			cout << "Need " << numCoins << " coins to construct amount [" << amount << "] cents." << endl;
+	}
 }
