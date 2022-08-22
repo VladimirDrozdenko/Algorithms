2bb572814df39bb60dbbfb97274ec430fbe138b7
VladimirDrozdenko <VladimirDrozdenko@users.noreply.github.com>
2022-08-22 11:02:35 -0400

    Iterative way of validating if tree is BST

--- ValidBinarySearchTree/tree.h
+++ ValidBinarySearchTree/tree.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <limits>
+#include <stack>
 #include <string>
 
 struct Node {
@@ -12,7 +13,7 @@ struct Node {
     Node(int val, Node* pl = nullptr, Node* pr = nullptr) 
         : value(val), left(pl), right(pr) {}
 
-    static void deleteTree(Node* root) {
+    static void deleteTree(const Node* root) {
         if (root) {
             deleteTree(root->left);
             deleteTree(root->right);
@@ -20,7 +21,7 @@ struct Node {
         }
     }
 
-    static void print(Node* root, std::string prefix = "") {
+    static void print(const Node* root, std::string prefix = "") {
         if (root) {
             std::cout << prefix << root->value << std::endl;
             print(root->left, prefix + "  ");
@@ -28,14 +29,44 @@ struct Node {
         }
     }
 
-    static bool isBST(Node* root) {
+    static bool recursiveIsBST(const Node* root) {
         return helperIsBST(root, 
             std::numeric_limits<int>::min(), 
             std::numeric_limits<int>::max());
     }
 
+    static bool isBST(const Node* root) {
+        std::stack<const Node*> s;
+
+        for (const Node* p = root; p; p = p->left) {
+            s.push(p);
+        }
+
+        const Node* prev = nullptr;
+
+        while(!s.empty()) {
+            const Node* current = s.top();
+
+            if (prev) {
+                if (prev->value >= current->value) {
+                    return false;
+                }    
+            }
+
+            prev = current;
+            
+            s.pop();
+
+            for (const Node* pp = current->right; pp; pp = pp->left) {
+                s.push(pp);
+            }
+        }
+
+        return true;
+    }
+
 private:
-    static bool helperIsBST(Node* root, int min, int max) {
+    static bool helperIsBST(const Node* root, int min, int max) {
         if (root == nullptr) {
             return true;
         }
